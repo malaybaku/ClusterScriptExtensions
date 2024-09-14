582f0b02e3e76e94a5dcb045ca90252c4e41c8c5
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2024-09-14 16:49:09 +0900

    Add component updater for additional types

---
+++ Assets/Baxter/ClusterScriptExtensions/Editor/ComponentUpdater/ItemMaterialSetListUpdater.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using ClusterVR.CreatorKit.Item;
+using ClusterVR.CreatorKit.Item.Implements;
+using UnityEditor;
+using UnityEngine;
+
+namespace Baxter.ClusterScriptExtensions.Editor.ComponentUpdater
+{
+    public static class ItemMaterialSetListUpdater
+    {
+        public static void Update(ScriptableItemExtension ext)
+        {
+            var elements = ext.ExtensionFields
+                .Where(f => f.Type is ExtensionFieldType.Material)
+                .Select(f => (Id: f.FieldName, Material: f.MaterialValue))
+                .ToArray();
+
+            var component = ext.GetComponent<ItemMaterialSetList>();
+
+            // コンポーネントが要らない場合、削除した状態を正とする
+            if (elements.Length == 0)
+            {
+                if (component != null)
+                {
+                    Object.DestroyImmediate(component);
+                    EditorUtility.SetDirty(ext.gameObject);
+                }
+                return;
+            }
+
+            // 必要なら普通に追加
+            if (component == null)
+            {
+                component = ext.gameObject.AddComponent<ItemMaterialSetList>();
+            }
+
+            var contents = elements
+                .Select(elem => new ItemMaterialSet(elem.Id, elem.Material))
+                .ToArray();
+            component.Construct(contents);
+        }
+    }    
+}
---
+++ Assets/Baxter/ClusterScriptExtensions/Editor/ComponentUpdater/WorldItemTemplateListUpdater.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using ClusterVR.CreatorKit.Item.Implements;
+using UnityEditor;
+using UnityEngine;
+
+namespace Baxter.ClusterScriptExtensions.Editor.ComponentUpdater
+{
+    public static class WorldItemTemplateListUpdater
+    {
+        public static void Update(ScriptableItemExtension ext)
+        {
+            var entries = ext.ExtensionFields
+                .Where(f => f.Type is ExtensionFieldType.AudioClip)
+                .Select(f => (Id: f.FieldName, Item: f.ItemReferenceValue))
+                .ToArray();
+
+            var component = ext.GetComponent<WorldItemTemplateList>();
+
+            // コンポーネントが要らない場合、削除した状態を正とする
+            if (entries.Length == 0)
+            {
+                if (component != null)
+                {
+                    Object.DestroyImmediate(component);
+                    EditorUtility.SetDirty(ext.gameObject);
+                }
+                return;
+            }
+
+            // 必要なら普通に追加
+            if (component == null)
+            {
+                component = ext.gameObject.AddComponent<WorldItemTemplateList>();
+            }
+
+            // public propertyがないので、シリアライズした内容ベースでリストを構築する
+            var so = new SerializedObject(component);
+            var entriesProperty = so.FindProperty("worldItemTemplates");
+            entriesProperty.ClearArray();
+            for (var i = 0; i < entries.Length; i++)
+            {
+                entriesProperty.InsertArrayElementAtIndex(i);
+                var elem = entriesProperty.GetArrayElementAtIndex(i);
+                elem.FindPropertyRelative("id").stringValue = entries[i].Id;
+                elem.FindPropertyRelative("worldItemTemplate").objectReferenceValue = entries[i].Item;
+            }
+            so.ApplyModifiedProperties();
+        }
+    }
+}
