4656066a48afc16d0ed005662739836af3242972
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2024-09-14 17:23:32 +0900

    Add sample script and assets

--- Assets/Internal/ClusterScripts/Item/Sample05_AssetReferences.js
+++ Assets/Internal/ClusterScripts/Item/Sample05_AssetReferences.js
@@ -4,6 +4,16 @@ const myAudio = $.audio("");
 // @field(HumanoidAnimation)
 const myMotion = $.humanoidAnimation("");
 
+// @field(WorldItem)
+const otherItemInstance = $.worldItemReference("");
+
+// @field(WorldItemTemplate)
+const creatableItem = new WorldItemTemplateId("");
+
+// @field(Material)
+const myRendererMaterial = $.material("");
+
+
 $.onInteract(() => { 
   $.log("try play audio...");
   myAudio.play();
