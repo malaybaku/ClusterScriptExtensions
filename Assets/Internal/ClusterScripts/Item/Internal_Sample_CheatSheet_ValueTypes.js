2288b984356ada3b0b27549bb432ea5455ee44e7
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2024-09-16 01:17:22 +0900

    Add internal scripts which are only for how inspector works

---
+++ Assets/Internal/ClusterScripts/Item/Internal_Sample_CheatSheet_ValueTypes.js
@@ -0,0 +1,20 @@
+// @field(bool)
+const myBool = false;
+// @field(int)
+const myInt = 10;
+// @field(int), @range(-10, 10)
+const myRangeInt = 0;
+// @field(float)
+const myFloat = 2.3;
+// @field(float), @range(-1.5, 1.5)
+const myRangeFloat = 0.0;
+// @field(string)
+const myString = "Test";
+// @field(string), @textArea
+const myLongString = "Test Long Text";
+// @field(Vector2)
+const myVector2 = new Vector2(1, 2);
+// @field(Vector3)
+const myVector3 = new Vector3(3, 4, 5);
+// @field(Quaternion)
+const myQuaternion = new Quaternion();
